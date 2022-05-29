bd7de30e4471edc561b185d20e066b1629ed1093
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-29 16:15:51 +0600

    react native style,,and textinput usese

--- components/Countries.js
+++ components/Countries.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView} from 'react-native'
+import { View, Text, ScrollView,TextInput ,StyleSheet} from 'react-native'
 import React, {useState,useEffect} from 'react'
 
 import Country from './Country'
@@ -12,10 +12,27 @@ export default function Countries() {
     },[])
   return (
     <View>
-      <Text>Countries length {countries.length} </Text>
+      <Text style={styles.header}>Countries length {countries.length} </Text>
+      <TextInput
+        style={styles.input}
+      >
+      </TextInput>
       <ScrollView>
       {countries.map(country=><Country country={country}></Country>)}
       </ScrollView>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  header:{
+    fontSize:40,
+    marginTop:40,
+    color:'red'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+})
